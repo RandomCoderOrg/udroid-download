1658a352f670428c3d5bc519e1ae052d2d0a3e16
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2022-05-21 17:19:50 +0530

    Add setup scripts and submodules

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+die()    { echo -e "[E] ${*}";exit 1;:;}
+warn()   { echo -e "[W] ${*}";:;}
+shout()  { echo -e "[-] ${*}";:;}
+lshout() { echo -e "-> ${*}";:;}
+msg()    { echo -e "${*} \e[0m" >&2;:;}
+
+DEPS="debootstrap qemu-user-static binfmt-support git"
+
+# ROOT user check
+if ((UID != 0 )); then
+    die "please run this script as root"
+else
+    SUDO=$(which sudo)
+fi
+
+# Force install dependencies
+shout "resolving Dependencies"
+$SUDO apt update || {
+    die "Failed to update indexes"
+}
+$SUDO apt install -y "$DEPS" || {
+    die "Failed to install packages.."
+}
+
+# pull submodules
+shout "Trying to update submodules.."
+git submodule init
+git submoudule update || {
+    die "Failed to pull submodules."
+}
+
+shout "DONE."
