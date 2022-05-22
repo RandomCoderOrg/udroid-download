dc3bdb812d31b76816652526c5d33d066670abd7
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2022-05-22 10:43:02 +0530

    Add build.sh [TODO]

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# ///////////////////////////
+# TODO
+
+# BUILDER BP
+# To Build
+# * ${0} [-s|--suite] <suite_name> [-v|--varient] <varient_name> [-a|--arch] <arch_name> 
+#
+# To List
+# * ${0} [-s|--suite] <suite_name> [-l|--list]
+# * ${0} [-s|--suite] <suite_name> [-v|--varient] <varient_name> [-l|--list]
+#
+# To show Help
+# * ${0} [-h|--help]
+#
+# To show version
+# * ${0} [-v|--version]
+#
+# ///////////////////////////
