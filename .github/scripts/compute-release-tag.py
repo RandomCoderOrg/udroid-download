6ebc09c80205cf939ebcc6787ed073e6ae9d3a35
DARK <80549753+Ayon-SSP@users.noreply.github.com>
2023-01-10 15:36:09 +0530

    Updated

--- .github/scripts/compute-release-tag.py
+++ .github/scripts/compute-release-tag.py
@@ -7,23 +7,19 @@ headers = {
     "Content-Type": "application/x-www-form-urlencoded"
 }
 
-
 API_ENDPOINT_UOA="https://api.github.com/repos/RandomCoderOrg/ubuntu-on-android/releases"
 resp_uoa = requests.get(API_ENDPOINT_UOA, headers=headers)
 Json_data2 = json.loads(resp_uoa.text)
 
-uV = Json_data2[0]['tag_name']
 # uV = ["v3.1-alpha"][0]   # For testing
-udroid_version = uV.replace("v","")
-
+udroid_version = Json_data2[0]['tag_name'].replace("v","")
 
 API_ENDPOINT_UA="https://api.github.com/repos/RandomCoderOrg/udroid-download/releases"
 resp_ud = requests.get(API_ENDPOINT_UA, headers=headers)
 Json_data = json.loads(resp_ud.text)
-dr = Json_data[0]['tag_name']
 
 
-match = re.compile(r'V(\d+)(\D+)(\d+)').findall(dr)
+match = re.compile(r'V(\d+)(\D+)(\d+)').findall(Json_data[0]['tag_name'])
 _, RELEASE_TYPE, IterationNumber = match[0]
 IterationNumber = int(IterationNumber) + 1
 
