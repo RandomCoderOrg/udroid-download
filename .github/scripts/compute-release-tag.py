8912c4762aef92211373e14e7635b0df85c905b6
DARK <80549753+Ayon-SSP@users.noreply.github.com>
2023-01-10 12:10:16 +0530

    RESOLVED Issue #7

--- .github/scripts/compute-release-tag.py
+++ .github/scripts/compute-release-tag.py
@@ -3,28 +3,36 @@ import json, requests, re
 
 GITHUB_ENV_PATH = os.getenv("GITHUB_ENV")
 
-API_ENDPOINT="https://api.github.com/repos/RandomCoderOrg/udroid-download/releases"
-
 headers = {
     "Content-Type": "application/x-www-form-urlencoded"
 }
 
-resp = requests.get(API_ENDPOINT, headers=headers)
-Json_data = json.loads(resp.text)
+
+API_ENDPOINT_UOA="https://api.github.com/repos/RandomCoderOrg/ubuntu-on-android/releases"
+resp_uoa = requests.get(API_ENDPOINT_UOA, headers=headers)
+Json_data2 = json.loads(resp_uoa.text)
+
+udroid_version = Json_data2[0]['tag_name']
+udroid_version = udroid_version.replace("v","")
+workType = Json_data2[0]['target_commitish']
 
 
+
+API_ENDPOINT_UA="https://api.github.com/repos/RandomCoderOrg/udroid-download/releases"
+resp_ud = requests.get(API_ENDPOINT_UA, headers=headers)
+Json_data = json.loads(resp_ud.text)
+dr = Json_data[0]['tag_name']
+
+
+match = re.compile(r'V(\d+)(\D+)(\d+)').findall(dr)
+_, RELEASE_TYPE, IterationNumber = match[0]
+IterationNumber = int(IterationNumber) + 1
 
 BUILD_TYPE="AB"
 
-# Geting the latest release tag name
-lR = Json_data[0]['tag_name']
-pattern = re.compile(r'V(\d+)(\D+)(\d+)')
-match = pattern.findall(lR)
-udroid_version, RELEASE_TYPE, udroid_download = match[0]
-udroid_download = int(udroid_download) + 1
-
-# Latest_release_tag = f"V{udroid_version}{RELEASE_TYPE}{udroid_download}"
-Latest_release_tag = "V%s%s%s%s" % (udroid_version,BUILD_TYPE,RELEASE_TYPE,udroid_download)
+Latest_release_tag = "V-%s-%s-%s%s%s" % (udroid_version,workType,BUILD_TYPE,RELEASE_TYPE,IterationNumber)
 
 with open(GITHUB_ENV_PATH, "a+") as f:
   f.write(Latest_release_tag)
+
+# print(Latest_release_tag)
