91272c6494697994bd163c13036a445c04cd2e9c
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2024-12-26 22:11:07 +0530

    chore: remove old end-of-life suites

--- suites/kinetic/gnomeRaw/gnomeRaw-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get install --no-install-recommends gnome-shell gnome-control-center-*  dbus-x11 xterm tigervnc-xorg-extension -y
-apt-get clean
--- suites/lunar/gnomeRaw/gnomeRaw-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get install --no-install-recommends gnome-shell dbus-x11 xterm tigervnc-xorg-extension -y
-apt-get clean
